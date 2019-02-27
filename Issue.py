d7464fc6f13933ad8c8f8570083d25d052e1e833
Jesse Hill <j_hill14@u.pacific.edu>
2019-02-27 13:42:32 -0800

    Issue and Topic classes created

---
+++ Issue.py
@@ -0,0 +1,10 @@
+class issue:
+
+    def __init__(self):
+        self.Title = None
+        self.link_out = None
+        self.text =None
+        self.img = None
+        self.Video = None
+
+
---
+++ Topic.py
@@ -0,0 +1,8 @@
+from Issue import issue
+
+class topic:
+
+    def __init__(self):
+        self.Issue = 0
+        self.issue_limit = 10
+
